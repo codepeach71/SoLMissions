dc7082d89458f76807203ac35f116e9d9325151d
codepeach71 <codepeach71@gmail.com>
2014-12-09 19:06:43 +1300

    new action object

--- src/sol/missions/Action.java
+++ src/sol/missions/Action.java
@@ -1,38 +1,95 @@
 package sol.missions;
 
+import java.util.*;
+
 public class Action {
 
 	private static Player player;
 	private Person thisPerson;
 	
-	private boolean bribeSpecialEnabled;
-	private int bribeSpecialMod;
+	private enum ActionType { BRIBE, KIDNAP, SNIPER }
+	private enum RewardType { COMPROMISE, TREASURE, PARANOIA };	
 	
-	public void setBribeSpecial(int mod, int costMoney, int costMedia) {
-		bribeSpecialEnabled = false;
-		bribeSpecialMod = mod;
-	}
+	private Map<ActionType, Integer> costs;
+	private Map<ActionType, Integer> mods;
+	private Map<ActionType, ActionResult> rewards;
+	private Map<ActionType, ActionResult> failurePenalties;
+	private Map<ActionType, Boolean> disabled;
 	
 	public Action(Person inPerson) {
-		thisPerson = inPerson;		
-		bribeSpecialEnabled = false;
+		thisPerson = inPerson;
+		
+		// initialise maps
+		costs = new HashMap<ActionType, Integer>();
+		mods = new HashMap<ActionType, Integer>();
+		rewards = new HashMap<ActionType, ActionResult>();
+		failurePenalties = new HashMap<ActionType, ActionResult>();
+		disabled = new HashMap<ActionType, Boolean>();
+		
+		// set default values for actions
+		// bribe
+		costs.put(ActionType.BRIBE, 1000);
+		mods.put(ActionType.BRIBE, 0);
+		rewards.put(ActionType.BRIBE, new ActionResult(RewardType.COMPROMISE, 20));
+		failurePenalties.put(ActionType.BRIBE, new ActionResult(RewardType.PARANOIA, -10));
+		disabled.put(ActionType.BRIBE, false);
+		
+		// kidnap
+		costs.put(ActionType.KIDNAP, 1000);
+		mods.put(ActionType.KIDNAP, 0);
+		rewards.put(ActionType.KIDNAP, null);
+		failurePenalties.put(ActionType.KIDNAP, null);
+		disabled.put(ActionType.KIDNAP, false);
+		
 	}
 	
 	public void setPlayer(Player p) {
 		player = p;
 	}
 	
+	/*
+	 * Rewards/Penalties
+	 */
+	
+	private class ActionResult {
+		private RewardType type;
+		private int value;
+		
+		private ActionResult(RewardType type, int value) {
+			this.type = type;
+			this.value = value;
+		}
+	}
+	
+	private void processResult(ActionResult reward) {
+		switch(reward.type) {
+		case COMPROMISE:
+			thisPerson.updateStatusCompromised(reward.value);
+			break;
+		case TREASURE:
+			// add to player treasure
+			break;
+		case PARANOIA:
+			thisPerson.updateStatusParanoia(reward.value);
+			break;
+		}
+	}
+	
+	/*
+	 * Actions
+	 */
+	
 	public boolean canBribe() {
-		return player.getAvailableMoney() >= thisPerson.getBribeCost();		
+		return !disabled.get(ActionType.BRIBE) && player.getAvailableMoney() >= thisPerson.getBribeCost();		
 	}
 
 	public void bribe() {
 		if (canBribe()) {
 			if (DCheck.beatDC(DiceBag.qRoll(1, 20, thisPerson.getModifiers().getManipulation()), thisPerson.getStatMorality())) {
-				thisPerson.updateStatusCompromised(20);
+				processResult(rewards.get(ActionType.BRIBE));
 			}
 			else {
-				thisPerson.updateStatusParanoia(10);
+				processResult(failurePenalties.get(ActionType.BRIBE));
 			}
 		}		
 	}
--- src/sol/missions/SpecialIntel.java
+++ src/sol/missions/SpecialIntel.java
@@ -3,7 +3,7 @@ package sol.missions;
 public class SpecialIntel extends Intel {
 	Person targetPerson;
 	
-	public SpecialIntel(String name, String contents, Person targetPerson) {
+	public SpecialIntel(String name, String contents, Person targetPerson, String actionType, int cost, String outcome, RollModifier modifiers) {
 		super(name, contents);
 		this.targetPerson = targetPerson;
 	}		
